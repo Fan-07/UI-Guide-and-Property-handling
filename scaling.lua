d7120db239695b1614bb50d0819449b10b57f5e6
fan <kuikelprabhav@gmail.com>
2026-06-01 22:24:34 +0545

    Add comment for customizable button scale size
    Added a comment to specify that users can choose their own scale size for the button.

---
+++ scaling.lua
@@ -0,0 +1,11 @@
+local TweenService = game:GetService("TweenService")
+local button = script.Parent
+
+button.MouseButton1Click:Connect(function()
+    local bigInfo = TweenInfo.new(0.1)
+    local smallInfo = TweenInfo.new(0.1)
+
+    TweenService:Create(button, bigInfo, {Size = UDim2.new(0.35, 0, 0.15, 0)}):Play()  --you can choose your own scale size
+    task.wait(0.1)
+    TweenService:Create(button, smallInfo, {Size = UDim2.new(0.3, 0, 0.12, 0)}):Play()
+end)
